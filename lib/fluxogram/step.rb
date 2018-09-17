f36d7efa8c7791445669e53281c206e2adda1a8d
David Moseler <davidmoseler@protonmail.com>
2018-09-17 14:13:12 -0300

    Fix parameter handling in Step

--- lib/fluxogram/step.rb
+++ lib/fluxogram/step.rb
@@ -1,7 +1,13 @@
 class Step < Node
   attr_accessor :process
 
-  def process(request)
-    @process.call(request) if @process
+  def process(request, params=nil)
+    if @process
+      if params
+        @process.call(request, params)
+      else
+        @process.call(request)
+      end
+    end
   end
 end
